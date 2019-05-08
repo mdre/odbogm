22a1b660e67ebcc22180bb943cea1964af601a9e
Juan Bertinetti <juanbertinetti@gmail.com>
2019-05-08 08:49:19 -0300

    Mejoras en DbManager

--- src/main/java/net/odbogm/DbManager.java
+++ src/main/java/net/odbogm/DbManager.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package net.odbogm;
 
 import com.tinkerpop.blueprints.impls.orient.OrientGraph;
@@ -34,29 +29,26 @@ import net.odbogm.annotations.IgnoreClass;
 import net.odbogm.annotations.Indexed;
 
 /**
- * DbManager se encarga de analizar todas las clases que se encuentren en los paquetes que se le indiquen o las clases específicas que se le indiquen
- * y crea la correspondiete estructura en la base de datos. Para ello se basa en las anotaciones:
+ * DbManager se encarga de analizar todas las clases que se encuentren en los
+ * paquetes que se le indiquen o las clases específicas que se le indiquen
+ * y crea la correspondiente estructura en la base de datos. Para ello se basa 
+ * en las anotaciones:
  *
- * {@code @Index}: crea un índice sobre el campo.
- * {@code @FieldAttributes}: define varios attributos de los campos de acuerdo a la cláusula {@code ALTER PROPERTY LINKEDCLASS}, the linked class name. Accepts a
- * string as value. NULL to remove it LINKEDTYPE, the linked type name between those supported:Types. Accepts a string as value. NULL to remove it
- * MIN, the minimum value as constraint. Accepts strings, numbers or dates as value. NULL to remove it MANDATORY, true if the property is mandatory.
- * Accepts "true" or "false" MAX, the maximum value as constraint. Accepts strings, numbers or dates as value. NULL to remove it NAME, the property
- * name. Accepts a string as value NOTNULL, the property can't be null. Accepts "true" or "false" REGEXP the regular expression as constraint. Accepts
- * a string as value. NULL to remove it TYPE, the type between those supported:Types Accepts a string as value COLLATE, sets the collate to define the
- * strategy of comparison. By default is case sensitive. By setting it yo "ci", any comparison will be case-insensitive READONLY the property value is
- * immutable: it can't be changed after the first assignment. Use this with DEFAULT to have immutable values on creation. Accepts "true" or "false"
- * CUSTOM Set custom properties. Syntax is {@code <name> = <value>}. Example: {@code stereotype = icon DEFAULT} (Since 2.1) set the default value. Default value can
- * be a value or a function. See below for examples
- * {@code @IgnoreProperty}: ignora el campo en la definición de la clase. Sin embargo se persistirá si el campo es distinto de null cuando se realice un store
- * del objeto.
+ * {@code @Indexed}: crea un índice sobre el campo.
+ * 
+ * {@code @FieldAttributes}: define varios atributos de los campos de acuerdo a
+ * la cláusula {@code ALTER PROPERTY}.
+ * 
+ * {@code @Ignore}: ignora el campo en la definición de la clase. Sin embargo se
+ * persistirá si el campo es distinto de null cuando se realice un store del objeto.
+ * 
+ * {@code @IgnoreClass}: ignora la clase completa.
  *
  * @author Marcelo D. Ré {@literal <marcelo.re@gmail.com>}
  */
 public class DbManager {
 
     private final static Logger LOGGER = Logger.getLogger(DbManager.class.getName());
-
     static {
         LOGGER.setLevel(LogginProperties.DbManager);
     }
@@ -69,20 +61,55 @@ public class DbManager {
     private static final char DIR_SEPARATOR = '/';
     private static final String CLASS_FILE_SUFFIX = ".class";
 
-    private ArrayList<ClassStruct> orderedRegisteredClass = new ArrayList<>();
-    private ConcurrentHashMap<String, ClassStruct> registeredClass = new ConcurrentHashMap<>();
+    private final ArrayList<ClassStruct> orderedRegisteredClass = new ArrayList<>();
+    private final ConcurrentHashMap<String, ClassStruct> registeredClasses = new ConcurrentHashMap<>();
 
     /** determina si se genera la cadena de drops como comentarios o no */
     private boolean withDrops = false;
     
+    /** determina si los comandos tienen en cuenta la existencia previa de los elementos */
+    private boolean incremental = true;
+    
+    
+    public DbManager() {
+    }
+    
+    /**
+     * 
+     * @param withDrops
+     * @param incremental 
+     */
+    public DbManager(boolean withDrops, boolean incremental) {
+        this.withDrops = withDrops;
+        this.incremental = incremental;
+    }
+    
+    /**
+     * 
+     * @param url
+     * @param user
+     * @param passwd
+     * @deprecated No se usa para nada la conexión a la base.
+     */
+    @Deprecated
     public DbManager(String url, String user, String passwd) {
         this.init(url, user, passwd);
     }
 
+    /**
+     * 
+     * @param url
+     * @param user
+     * @param passwd
+     * @param withDrops 
+     * @deprecated No se usa para nada la conexión a la base.
+     */
+    @Deprecated
     public DbManager(String url, String user, String passwd, boolean withDrops) {
         this.init(url, user, passwd);
         this.withDrops = withDrops;
     }
+
 
     private void init(String url, String user, String passwd){
         this.factory = new OrientGraphFactory(url, user, passwd).setupPool(1, 10);
@@ -93,42 +120,17 @@ public class DbManager {
     }
 
     public DbManager setClassLevelLog(Class<?> clazz, Level level) {
-        Logger L = LOGGER.getLogger( clazz.getName() );
+        Logger L = Logger.getLogger(clazz.getName());
         L.setLevel(level);
         return this;
     }
     
-    /**
-     * recorrer el vector analizando todas las clases que se encuentran referenciadas y crea
-     * las sentencias correspondientes para su creación            
-     */
-    private void process(String[] analize) {
-        List<Class<?>> classes = new ArrayList<>();
-        for (String clazz : analize) {
-            classes.addAll(find(clazz));
-        }
-        // levantar cada clase y verificar su existencia en la base.
-        for (Class<?> clazz : classes) {
-            // verificar que la clase exista en la base.
-            String className = clazz.getSimpleName();
-            if (clazz.isAnnotationPresent(Entity.class))
-                buildDBScript(clazz);
-        }
-        
-//        for (ClassStruct orderedRegisteredClas : orderedRegisteredClass) {
-//            System.out.println(orderedRegisteredClas.drop);
-//            System.out.println(orderedRegisteredClas.create);
-//            for (String property : orderedRegisteredClas.properties) {
-//                System.out.println(property);
-//            }
-//            System.out.println("");
-//        }
-    }
-
     public void generateToConsole(String[] analize){
         this.process(analize);
         for (ClassStruct orderedRegisteredClas : orderedRegisteredClass) {
             System.out.println(orderedRegisteredClas.drop);
+        }
+        for (ClassStruct orderedRegisteredClas : orderedRegisteredClass) {
             System.out.println(orderedRegisteredClas.create);
             for (String property : orderedRegisteredClas.properties) {
                 System.out.println(property);
@@ -149,8 +151,9 @@ public class DbManager {
         ArrayList<String> statements = new ArrayList<>();
         this.process(analize);
         for (ClassStruct orderedRegisteredClas : orderedRegisteredClass) {
-            
             statements.add(orderedRegisteredClas.drop);
+        }
+        for (ClassStruct orderedRegisteredClas : orderedRegisteredClass) {
             statements.add(orderedRegisteredClas.create);
             for (String property : orderedRegisteredClas.properties) {
                 statements.add(property);
@@ -158,7 +161,6 @@ public class DbManager {
             for (String index : orderedRegisteredClas.classIndexes) {
                 statements.add(index);
             }
-            
         }
         return statements;
     }
@@ -177,9 +179,11 @@ public class DbManager {
             fw = new FileWriter(fileName);
             LOGGER.log(Level.FINER, "preparando el printwriter...");
             pw = new PrintWriter(fw);
-            LOGGER.log(Level.FINER, "procesando "+orderedRegisteredClass.size()+" lineas...");
+            LOGGER.log(Level.FINER, "procesando {0} lineas...", orderedRegisteredClass.size());
             for (ClassStruct orderedRegisteredClas : orderedRegisteredClass) {
                 pw.println(orderedRegisteredClas.drop);
+            }
+            for (ClassStruct orderedRegisteredClas : orderedRegisteredClass) {
                 pw.println(orderedRegisteredClas.create);
                 for (String property : orderedRegisteredClas.properties) {
                     pw.println(property);
@@ -200,6 +204,7 @@ public class DbManager {
             }
         }
     }
+    
     /**
      * Verifica que el árbol de herencias de la clase esté registrado. Si no es así, lo registra desde la clase superior hacia abajo.
      *
@@ -220,38 +225,45 @@ public class DbManager {
         if (clazz.getSuperclass() != Object.class) {
             buildDBScript(clazz.getSuperclass());
             superName = (clazz.getSuperclass()==null?"":clazz.getSuperclass().getSimpleName());
-        } 
-            
+        }
+        
         
         // procesar todos los campos de la clase actual.
         String className = clazz.getSimpleName();
 
         // verificar si ya se ha agregado
-        if (this.registeredClass.get(className) != null) {
+        if (this.registeredClasses.get(className) != null) {
             return;
         }
         // la clase no existe aún. Registrarla
         ClassStruct clazzStruct = new ClassStruct(className);
-        this.registeredClass.put(className, clazzStruct);
+        this.registeredClasses.put(className, clazzStruct);
         this.orderedRegisteredClass.add(clazzStruct);
-        // preparar orden de dropeo. Solo se ejecuta si la clase existe
-        clazzStruct.drop = (!this.withDrops?"/*\n":"\n")
-                +"let exist = select from (select expand(classes) from metadata:schema) where name = '"+className+"';\n"
-                + "if ($exist.size()>0) {\n"
-                + "     delete vertex "+className+";\n"
-                + "     drop class "+className+";\n"
-                + "}"
-                + (!this.withDrops?"\n */":"\n");
         
-        // order de create. Solo se ejecuta si no existe la clase
-        clazzStruct.create = "\n"
-                +"let exist = select from (select expand(classes) from metadata:schema) where name = '"+className+"';\n"
-                + "if ($exist.size()=0) {\n"
-                + "     create class "+className+(superName.isEmpty()?" extends V":" extends "+superName)+";\n"
-                + "}"
-                + "\n "
-                + "alter class "+className+" custom javaClass='"+clazz.getCanonicalName()+"';\n"
-                + (superName.isEmpty()?" ":"alter class "+className+" superclass "+superName+";\n");
+        String exist = "let exist = select from (select expand(classes) from "
+                + "metadata:schema) where name = '"+className+"';\n"
+                + "if ($exist.size() %s 0) {\n"
+                + "    %s"
+                + "}\n";
+        
+        // orden de drop
+        
+        String drop = "delete vertex " + className + ";\n"
+                + "    drop class " + className + ";\n";
+        
+        clazzStruct.drop = (!this.withDrops?"/*\n":"")
+                + String.format(exist, ">", drop) + (!this.withDrops?"*/":"");
+        
+        // orden de create
+        
+        String create = "create class " + className + (superName.isEmpty() ?
+                " extends V" : " extends " + superName) + ";\n";
+        
+        clazzStruct.create = (this.incremental ? String.format(exist, "=", create) : create)
+                + "alter class " + className + " custom javaClass='" 
+                + clazz.getCanonicalName() + "';\n"
+                + (superName.isEmpty() ? "" : "alter class " + className 
+                + " superclass " + superName + ";\n");
 
         // se utilizará para registrar todas las clases de atributos que no sean primitivos
         ArrayList<Class<?>> postProcess = new ArrayList<>();
@@ -294,15 +306,18 @@ public class DbManager {
                     } else {
                         type = PRIMITIVE_MAP.get(field.getType()).toString();
                     }
-                    String statement = "\n"
+                    String statement = "create property " + currentProp + " " + type + ";";
+                    String let = "\n"
                                     +"let exist = select from "
                                                         + "(select expand(properties) "
                                                         + " from (select expand(classes) "
                                                         + " from metadata:schema) "
                                                         + " where name = '"+className+"') where name = '"+field.getName()+"';\n"
                                     + "if ($exist.size()=0) {\n"
-                                    + "     create property " + currentProp + " " + type +";"
+                                    + "    %s"
                                     + "\n}\n ";
+                    
+                    statement = this.incremental ? String.format(let, statement) : statement;
                     clazzStruct.properties.add(statement);
 
                     if (fa != null) {
@@ -359,18 +374,19 @@ public class DbManager {
                     // si no es un tipo básico, se debe conectar con un Edge.
 //                    if (Primitives.LAZY_COLLECTION.get(field.getType())!=null) {
                     // FIXME: ojo que si se tratara de una extensión de AL o HM no lo vería como tal y lo vincularía con un link
-                    clazzStruct.properties.add("\n"
+                    String createLink = "create class "+className+"_"+field.getName()+" extends E;";
+                    String statement = this.incremental ? String.format("\n"
                         +"let exist = select from (select expand(classes) from metadata:schema) where name = '"+className+"_"+field.getName()+"';\n"
                         + "if ($exist.size()=0) {\n"
-                        + "     create class "+className+"_"+field.getName()+" extends E;"
-                        + "\n}\n "
-                        );
+                        + "    %s"
+                        + "\n}\n ", createLink) : createLink;
+                    clazzStruct.properties.add(statement);
                     // y se debe analizar si no se trata de una clase marcada como Entidad
                     // que no se encuentra entre los paquetes indicados como parámetros
                     // Ej. UserSID / GroupSID
                     LOGGER.log(Level.FINER, "field type: "+field.getType());
                     if (field.getType().isAnnotationPresent(Entity.class) 
-                            && registeredClass.get(field.getType().getSimpleName())==null) {
+                            && registeredClasses.get(field.getType().getSimpleName())==null) {
                         postProcess.add(field.getType());
                     }
                 }
@@ -380,12 +396,13 @@ public class DbManager {
                     String engine = idx.type()==Indexed.IndexType.LUCENE?" FULLTEXT ENGINE LUCENE ":""+idx.type();
                     engine += (!idx.metadata().isEmpty()?"METADATA "+idx.metadata():"");
                     
-                    clazzStruct.properties.add("\n"
+                    String createLink = "create index "+currentProp+" on "+className+"("+field.getName()+") "+engine+";";
+                    String statement = this.incremental ? String.format("\n"
                         +"let exist = select from(select expand(indexes) from metadata:indexmanager) where name = '"+currentProp+"';\n"
                         + "if ($exist.size()=0) {\n"
-                        + "     create index "+currentProp+" on "+className+"("+field.getName()+") "+engine+";"
-                        + "\n}\n "
-                        );
+                        + "    %s"
+                        + "\n}\n ", createLink) : createLink;
+                    clazzStruct.properties.add(statement);
                 }
             }
         }
@@ -402,16 +419,34 @@ public class DbManager {
         }
     }
 
+    /**
+     * recorrer el vector analizando todas las clases que se encuentran referenciadas y crea
+     * las sentencias correspondientes para su creación            
+     */
+    private void process(String[] analize) {
+        List<Class<?>> classes = new ArrayList<>();
+        for (String clazz : analize) {
+            classes.addAll(find(clazz));
+        }
+        // levantar cada clase y verificar su existencia en la base.
+        for (Class<?> clazz : classes) {
+            // verificar que la clase exista en la base.
+            if (clazz.isAnnotationPresent(Entity.class))
+                buildDBScript(clazz);
+        }
+    }
+    
     private List<Class<?>> find(String scannedPackage) {
         String scannedPath = scannedPackage.replace(PKG_SEPARATOR, DIR_SEPARATOR);
         URL scannedUrl = Thread.currentThread().getContextClassLoader().getResource(scannedPath);
-        LOGGER.log(Level.INFO, "URL: "+scannedUrl);
+        LOGGER.log(Level.INFO, "URL: {0}", scannedUrl);
         if (scannedUrl == null) {
-            throw new IllegalArgumentException(String.format(BAD_PACKAGE_ERROR, scannedPath, scannedPackage));
+            throw new IllegalArgumentException(String.format(
+                    BAD_PACKAGE_ERROR, scannedPath, scannedPackage));
         }
         File scannedDir = new File(scannedUrl.getFile());
         try {
-            LOGGER.log(Level.INFO, "scannedDir: "+scannedDir.getCanonicalPath());
+            LOGGER.log(Level.INFO, "scannedDir: {0}", scannedDir.getCanonicalPath());
         } catch (IOException ex) {
             Logger.getLogger(DbManager.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -425,7 +460,7 @@ public class DbManager {
     private List<Class<?>> find(File file, String scannedPackage) {
         List<Class<?>> classes = new ArrayList<>();
         String resource = scannedPackage + PKG_SEPARATOR + file.getName();
-        LOGGER.log(Level.INFO, "resource: "+resource);
+        LOGGER.log(Level.INFO, "resource: {0}", resource);
         if (file.isDirectory()) {
             for (File child : file.listFiles()) {
                 classes.addAll(find(child, resource));
@@ -442,8 +477,9 @@ public class DbManager {
     }
 
     /**
-     * Clase de ayuda para crear los comandos que generan la base de datos. Genera los delete vertex y drops como comentarios
-     * para que sean desmarcados si se desea.
+     * Clase de ayuda para crear los comandos que generan la base de datos.
+     * Genera los delete vertex y drops como comentarios para que sean
+     * desmarcados si se desea.
      */
     class ClassStruct {
 
@@ -478,10 +514,7 @@ public class DbManager {
                 return false;
             }
             final ClassStruct other = (ClassStruct) obj;
-            if (!Objects.equals(this.className, other.className)) {
-                return false;
-            }
-            return true;
+            return Objects.equals(this.className, other.className);
         }
 
     }
--- src/main/java/net/odbogm/annotations/FieldAttributes.java
+++ src/main/java/net/odbogm/annotations/FieldAttributes.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package net.odbogm.annotations;
 
 import java.lang.annotation.ElementType;
@@ -11,23 +6,24 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * {@code @FieldAttributes}: define varios attributos de los campos de acuerdo a la cláusula ALTER PROPERTY
- *          LINKEDCLASS, the linked class name. Accepts a string as value. NULL to remove it
- *          LINKEDTYPE, the linked type name between those supported:Types. Accepts a string as value. NULL to remove it
- *          MIN, the minimum value as constraint. Accepts strings, numbers or dates as value. NULL to remove it
- *          MANDATORY, true if the property is mandatory. Accepts "true" or "false"
- *          MAX, the maximum value as constraint. Accepts strings, numbers or dates as value. NULL to remove it
- *          NAME, the property name. Accepts a string as value
- *          NOTNULL, the property can't be null. Accepts "true" or "false"
- *          REGEXP the regular expression as constraint. Accepts a string as value. NULL to remove it
- *          TYPE, the type between those supported:Types Accepts a string as value
- *          COLLATE, sets the collate to define the strategy of comparison. By default is case sensitive. 
- *                   By setting it yo "ci", any comparison will be case-insensitive
- *          READONLY the property value is immutable: it can't be changed after the first assignment. 
- *                   Use this with DEFAULT to have immutable values on creation. Accepts "true" or "false"
- *          CUSTOM Set custom properties. Syntax is {@code <name> = <value>}. Example: {@code stereotype = icon}
- *          DEFAULT (Since 2.1) set the default value. Default value can be a value or a function. See below for examples
+ * {@code @FieldAttributes}: define varios attributos de los campos de acuerdo a
+ * la cláusula ALTER PROPERTY.
  * 
+ * LINKEDCLASS, the linked class name. Accepts a string as value. NULL to remove it.
+ * LINKEDTYPE, the linked type name between those supported:Types. Accepts a string as value. NULL to remove it.
+ * MIN, the minimum value as constraint. Accepts strings, numbers or dates as value. NULL to remove it.
+ * MANDATORY, true if the property is mandatory. Accepts "true" or "false".
+ * MAX, the maximum value as constraint. Accepts strings, numbers or dates as value. NULL to remove it.
+ * NAME, the property name. Accepts a string as value.
+ * NOTNULL, the property can't be null. Accepts "true" or "false".
+ * REGEXP, the regular expression as constraint. Accepts a string as value. NULL to remove it.
+ * TYPE, the type between those supported:Types. Accepts a string as value.
+ * COLLATE, sets the collate to define the strategy of comparison. By default is case sensitive. 
+ *          By setting it yo "ci", any comparison will be case-insensitive.
+ * READONLY, the property value is immutable: it can't be changed after the first assignment. 
+ *          Use this with DEFAULT to have immutable values on creation. Accepts "true" or "false".
+ * CUSTOM, set custom properties. Syntax is {@code <name> = <value>}. Example: {@code stereotype = icon}
+ * DEFAULT, set the default value. Default value can be a value or a function.
  * 
  * @author Marcelo D. Ré {@literal <marcelo.re@gmail.com>}
  */
